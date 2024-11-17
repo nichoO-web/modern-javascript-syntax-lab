f75b8fde429137a7c56963ad2c3f42fb65cb2ba8
nichoOtero <nichonaks@gmail.com>
2024-11-17 14:43:07 -0600

    modern-javascript-syntax-lab

---
+++ app.js
@@ -0,0 +1,27 @@
+// Use `.map()` to iterate over the following array:
+
+const nums = [13, 87, 2, 89, 12, 4, 90, 63];
+
+// Create a new array where each value is multiplied by 2 and log the new array.
+
+// Your code here
+const nums2x = nums.map((num) => {
+    return num * 2;
+});
+
+console.log(nums2x);
+
+
+
+// Convert the following `if...else` statement in to a ternary:
+
+let pizza = 'tasty';
+
+if (pizza === 'tasty') {
+  console.log('yum');
+} else {
+  console.log('yuck');
+}
+
+// Your code here
+let tasty = pizza === 'tasty' ? 'yum' : 'yuck';
